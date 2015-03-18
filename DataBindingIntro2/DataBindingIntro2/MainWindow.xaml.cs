08b701118bc61e6e3cefddb37becf9375cc12cec
johanwintgens <peoplecallmeshady@hotmail.com>
2015-03-18 14:55:15 +0100

    init

---
+++ DataBindingIntro2/DataBindingIntro2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingIntro2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Student S1 = new Student();
+        private Student S2 = new Student();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            S1.Leeftijd = 20;
+            S1.Naam = "Fritske";
+
+            S2.Leeftijd = 21;
+            S2.Naam = "Bobke";
+
+            //VOLGENDE REGEL LINKT EEN OBJECT AAN DE TEXTBOX! Anders weet deze niet welk object, klasse, ... aan hem gebind moet worden
+            txbStudentInfo.DataContext = S1;
+            txbStudentInfo.DataContext = S2;
+        }
+
+        private void btnSwap_Click(object sender, RoutedEventArgs e)
+        {
+            txbStudentInfo.DataContext = S2;
+        }
+
+        private void btnToonInfo_Click(object sender, RoutedEventArgs e)
+        {
+            S1.ToonInfo();
+            S2.ToonInfo();
+        }
+    }
+}
---
+++ DataBindingIntro2/DataBindingIntro2/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DataBindingIntro2
+{
+    class Student
+    {
+        public Student()
+        {
+            deComputer.Merk = "Pear";
+            deComputer.Opslag = 500;
+        }
+
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+        public Computer deComputer { get; set; }
+
+        public void ToonInfo()
+        {
+            MessageBox.Show(Naam + " " + Leeftijd);
+        }
+    }
+
+    class Computer
+    {
+        public string Merk { get; set; }
+        public int Opslag { get; set; }
+    }
+}
